ba17897ce11f52f0b9e7e05a8dad04acb79fd551
csparpa <csparpa@gmail.com>
2014-10-14 17:08:28 +0100

    Fix missing var keywords

--- specs/timehaze.delta.ago-spec.js
+++ specs/timehaze.delta.ago-spec.js
@@ -12,283 +12,283 @@ describe("Timehaze.Delta.ago function", function() {
 
   it("should return 'right now'", function() {
     var timestamp = new Date(2000, 0, 1, 0, 0, 5, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("right now");
   });
 
   it("should return 'moments ago'", function() {
     var timestamp = new Date(2000, 0, 1, 0, 0, 15, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("moments ago");
   });
 
   it("should return 'in moments'", function() {
     var timestamp = new Date(1999, 11, 31, 23, 59, 45, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in moments");
   });
 
   it("should return 'a minute ago'", function() {
     var timestamp = new Date(2000, 0, 1, 0, 1, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a minute ago");
   });
 
   it("should return 'in a minute'", function() {
     var timestamp = new Date(1999, 11, 31, 23, 59, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a minute");
   });
 
   it("should return 'a few minutes ago'", function() {
     var timestamp = new Date(2000, 0, 1, 0, 10, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a few minutes ago");
   });
 
   it("should return 'in a few minutes'", function() {
     var timestamp = new Date(1999, 11, 31, 23, 50, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a few minutes");
   });
 
   it("should return 'half an hour ago'", function() {
     var timestamp = new Date(2000, 0, 1, 0, 30, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("half an hour ago");
   });
 
   it("should return 'in half an hour'", function() {
     var timestamp = new Date(1999, 11, 31, 23, 30, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in half an hour");
   });
 
   it("should return 'almost an hour ago'", function() {
     var timestamp = new Date(2000, 0, 1, 0, 45, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("almost an hour ago");
   });
 
   it("should return 'in almost an hour'", function() {
     var timestamp = new Date(1999, 11, 31, 23, 15, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in almost an hour");
   });
 
   it("should return 'an hour ago'", function() {
     var timestamp = new Date(2000, 0, 1, 1, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("an hour ago");
   });
 
   it("should return 'in an hour'", function() {
     var timestamp = new Date(1999, 11, 31, 23, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in an hour");
   });
 
   it("should return 'a few hours ago'", function() {
     var timestamp = new Date(2000, 0, 1, 5, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a few hours ago");
   });
 
   it("should return 'in a few hours'", function() {
     var timestamp = new Date(1999, 11, 31, 19, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a few hours");
   });
 
   it("should return 'almost half a day ago'", function() {
     var timestamp = new Date(2000, 0, 1, 10, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("almost half a day ago");
   });
 
   it("should return 'in almost half a day'", function() {
     var timestamp = new Date(1999, 11, 31, 14, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in almost half a day");
   });
 
   it("should return 'half a day ago'", function() {
     var timestamp = new Date(2000, 0, 1, 15, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("half a day ago");
   });
 
   it("should return 'in half a day'", function() {
     var timestamp = new Date(1999, 11, 31, 9, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in half a day");
   });
 
   it("should return 'a day ago'", function() {
     var timestamp = new Date(2000, 0, 2, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a day ago");
   });
 
   it("should return 'in a day'", function() {
     var timestamp = new Date(1999, 11, 31, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a day");
   });
 
   it("should return 'a few days ago'", function() {
     var timestamp = new Date(2000, 0, 4, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a few days ago");
   });
 
   it("should return 'in a few days'", function() {
     var timestamp = new Date(1999, 11, 28, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a few days");
   });
 
   it("should return 'almost a week ago'", function() {
     var timestamp = new Date(2000, 0, 6, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("almost a week ago");
   });
 
   it("should return 'in almost a week'", function() {
     var timestamp = new Date(1999, 11, 26, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in almost a week");
   });
 
   it("should return 'a week ago'", function() {
     var timestamp = new Date(2000, 0, 10, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a week ago");
   });
 
   it("should return 'in a week'", function() {
     var timestamp = new Date(1999, 11, 21, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a week");
   });
 
   it("should return 'a few weeks ago'", function() {
     var timestamp = new Date(2000, 0, 20, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a few weeks ago");
   });
 
   it("should return 'in a few weeks'", function() {
     var timestamp = new Date(1999, 11, 12, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a few weeks");
   });
 
   it("should return 'almost a month ago'", function() {
     var timestamp = new Date(2000, 0, 25, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("almost a month ago");
   });
 
   it("should return 'in almost a month'", function() {
     var timestamp = new Date(1999, 11, 6, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in almost a month");
   });
 
   it("should return 'a month ago'", function() {
     var timestamp = new Date(2000, 1, 10, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a month ago");
   });
 
   it("should return 'in a month'", function() {
     var timestamp = new Date(1999, 10, 10, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a month");
   });
 
   it("should return 'a few months ago'", function() {
     var timestamp = new Date(2000, 3, 10, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a few months ago");
   });
 
   it("should return 'in a few months'", function() {
     var timestamp = new Date(1999, 9, 10, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a few months");
   });
 
   it("should return 'almost a year ago'", function() {
     var timestamp = new Date(2000, 10, 10, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("almost a year ago");
   });
 
   it("should return 'in almost a year'", function() {
     var timestamp = new Date(1999, 1, 10, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in almost a year");
   });
 
   it("should return 'a year ago'", function() {
     var timestamp = new Date(2001, 1, 1, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a year ago");
   });
 
   it("should return 'in a year'", function() {
     var timestamp = new Date(1998, 11, 1, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a year");
   });
 
   it("should return 'a few years ago'", function() {
     var timestamp = new Date(2005, 1, 1, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a few years ago");
   });
 
   it("should return 'in a few years'", function() {
     var timestamp = new Date(1995, 11, 1, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a few years");
   });
 
   it("should return 'almost a decade ago'", function() {
     var timestamp = new Date(2009, 1, 1, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("almost a decade ago");
   });
 
   it("should return 'in almost a decade'", function() {
     var timestamp = new Date(1991, 11, 1, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in almost a decade");
   });
 
   it("should return 'a decade ago'", function() {
     var timestamp = new Date(2011, 2, 1, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a decade ago");
   });
 
   it("should return 'in a decade'", function() {
     var timestamp = new Date(1989, 2, 1, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a decade");
   });
 
   it("should return 'a long time ago' for very big timedeltas", function() {
     var timestamp = new Date(2050, 2, 1, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("a long time ago");
   });
 
   it("should return 'in a long time' for very big timedeltas", function() {
     var timestamp = new Date(1930, 2, 1, 0, 0, 0, 0);
-    res = (th.delta(eventDate, timestamp)).ago();
+    var res = (th.delta(eventDate, timestamp)).ago();
     expect(res).toBe("in a long time");
   });
 
--- specs/timehaze.delta.interval-spec.js
+++ specs/timehaze.delta.interval-spec.js
@@ -12,283 +12,283 @@ describe("Timehaze.Delta.interval function", function() {
 
     it("should return 'right now'", function() {
         var timestamp = new Date(2000, 0, 1, 0, 0, 5, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("right now");
     });
 
     it("should return 'last minute'", function() {
         var timestamp = new Date(2000, 0, 1, 0, 0, 15, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last minute");
     });
 
     it("should return 'next minute'", function() {
         var timestamp = new Date(1999, 11, 31, 23, 59, 45, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next minute");
     });
 
     it("should return 'last minute' again", function() {
         var timestamp = new Date(2000, 0, 1, 0, 1, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last minute");
     });
 
     it("should return 'next minute' again", function() {
         var timestamp = new Date(1999, 11, 31, 23, 59, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next minute");
     });
 
     it("should return 'last minutes'", function() {
         var timestamp = new Date(2000, 0, 1, 0, 10, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last minutes");
     });
 
     it("should return 'next minutes", function() {
         var timestamp = new Date(1999, 11, 31, 23, 50, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next minutes");
     });
 
     it("should return 'last half an hour'", function() {
         var timestamp = new Date(2000, 0, 1, 0, 30, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last half an hour");
     });
 
     it("should return 'next half an hour'", function() {
         var timestamp = new Date(1999, 11, 31, 23, 30, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next half an hour");
     });
 
     it("should return 'last half an hour'", function() {
         var timestamp = new Date(2000, 0, 1, 0, 45, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last half an hour");
     });
 
     it("should return 'next half an hour'", function() {
         var timestamp = new Date(1999, 11, 31, 23, 15, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next half an hour");
     });
 
     it("should return 'last hour'", function() {
         var timestamp = new Date(2000, 0, 1, 1, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last hour");
     });
 
     it("should return 'next hour'", function() {
         var timestamp = new Date(1999, 11, 31, 23, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next hour");
     });
 
     it("should return 'last hours'", function() {
         var timestamp = new Date(2000, 0, 1, 5, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last hours");
     });
 
     it("should return 'next hours'", function() {
         var timestamp = new Date(1999, 11, 31, 19, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next hours");
     });
 
     it("should return 'last hours' again", function() {
         var timestamp = new Date(2000, 0, 1, 10, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last hours");
     });
 
     it("should return 'next hours' again", function() {
         var timestamp = new Date(1999, 11, 31, 14, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next hours");
     });
 
     it("should return 'last hours' once again", function() {
         var timestamp = new Date(2000, 0, 1, 15, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last hours");
     });
 
     it("should return 'next hours' once again", function() {
         var timestamp = new Date(1999, 11, 31, 9, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next hours");
     });
 
     it("should return 'last day'", function() {
         var timestamp = new Date(2000, 0, 2, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last day");
     });
 
     it("should return 'next day'", function() {
         var timestamp = new Date(1999, 11, 31, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next day");
     });
 
     it("should return 'last days'", function() {
         var timestamp = new Date(2000, 0, 4, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last days");
     });
 
     it("should return 'next days'", function() {
         var timestamp = new Date(1999, 11, 28, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next days");
     });
 
     it("should return 'last week'", function() {
         var timestamp = new Date(2000, 0, 6, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last week");
     });
 
     it("should return 'next week'", function() {
         var timestamp = new Date(1999, 11, 26, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next week");
     });
 
     it("should return 'last weeks'", function() {
         var timestamp = new Date(2000, 0, 10, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last weeks");
     });
 
     it("should return 'next weeks'", function() {
         var timestamp = new Date(1999, 11, 21, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next weeks");
     });
 
     it("should return 'last weeks' again", function() {
         var timestamp = new Date(2000, 0, 20, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last weeks");
     });
 
     it("should return 'next weeks' again", function() {
         var timestamp = new Date(1999, 11, 12, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next weeks");
     });
 
     it("should return 'last weeks' once again", function() {
         var timestamp = new Date(2000, 0, 25, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last weeks");
     });
 
     it("should return 'next weeks' once again", function() {
         var timestamp = new Date(1999, 11, 6, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next weeks");
     });
 
     it("should return 'last month'", function() {
         var timestamp = new Date(2000, 1, 10, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last month");
     });
 
     it("should return 'next month'", function() {
         var timestamp = new Date(1999, 10, 10, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next month");
     });
 
     it("should return 'last months'", function() {
         var timestamp = new Date(2000, 3, 10, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last months");
     });
 
     it("should return 'next months'", function() {
         var timestamp = new Date(1999, 9, 10, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next months");
     });
 
     it("should return 'last months' again", function() {
         var timestamp = new Date(2000, 10, 10, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last months");
     });
 
     it("should return 'next months' again", function() {
         var timestamp = new Date(1999, 1, 10, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next months");
     });
 
     it("should return 'last year'", function() {
         var timestamp = new Date(2001, 1, 1, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last year");
     });
 
     it("should return 'next year'", function() {
         var timestamp = new Date(1998, 11, 1, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next year");
     });
 
     it("should return 'last years'", function() {
         var timestamp = new Date(2005, 1, 1, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last years");
     });
 
     it("should return 'next years'", function() {
         var timestamp = new Date(1995, 11, 1, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next years");
     });
 
     it("should return 'last years' again", function() {
         var timestamp = new Date(2009, 1, 1, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last years");
     });
 
     it("should return 'next years' again", function() {
         var timestamp = new Date(1991, 11, 1, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next years");
     });
 
     it("should return 'last decade'", function() {
         var timestamp = new Date(2011, 2, 1, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("last decade");
     });
 
     it("should return 'next decade'", function() {
         var timestamp = new Date(1989, 2, 1, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("next decade");
     });
 
     it("should return 'a long time ago' for very big timedeltas", function() {
         var timestamp = new Date(2050, 2, 1, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("a long time ago");
     });
 
     it("should return 'in a long time' for very big timedeltas", function() {
         var timestamp = new Date(1930, 2, 1, 0, 0, 0, 0);
-        res = (th.delta(eventDate, timestamp)).interval();
+        var res = (th.delta(eventDate, timestamp)).interval();
         expect(res).toBe("in a long time");
     });
 
